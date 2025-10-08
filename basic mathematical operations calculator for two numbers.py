142f92a6922a4928c70abff9d1e2113759d9cd36
Rishab Kumar <rishabkumar5470@gmail.com>
2025-10-09 01:34:44 +0530

    Add files via upload

---
+++ basic mathematical operations calculator for two numbers.py
@@ -0,0 +1,14 @@
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+
+a=int(a)
+b=int(b)
+x=(a+b)
+y=(a-b)
+z=(a*b)
+d=(a/b)
+
+print("addition: ",x)
+print("subtraction: ",y)
+print("multiplication: ",z)
+print("division: ",d)
